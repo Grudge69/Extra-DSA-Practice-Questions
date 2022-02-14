8fe940c9ced080fb91cfcc0aaff813387bb9fad0
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-15 04:40:39 +0530

    max depth bin tree RECURSION, LEETCODE SOLUTION

--- Max Depth of Binary Tree(LeetCode).java
+++ Max Depth of Binary Tree(LeetCode).java
@@ -15,8 +15,10 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        // base condition(at end of leaf node)
         if(root == null) return 0;
         
+        // add 1 to the sub answer of whichever is having max depth among left and right sub tree
         return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
